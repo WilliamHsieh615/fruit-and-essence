85748b9505cc2d01e6fc68d7a39dc430554ccbc8
William Hsieh <best5946@gmail.com>
2025-08-18 21:55:33 +0800

    初始化 Spring Boot project

---
+++ src/main/java/com/williamhsieh/fruitandessence/FruitAndEssenceApplication.java
@@ -0,0 +1,13 @@
+package com.williamhsieh.fruitandessence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitAndEssenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitAndEssenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/williamhsieh/fruitandessence/FruitAndEssenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamhsieh.fruitandessence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitAndEssenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
