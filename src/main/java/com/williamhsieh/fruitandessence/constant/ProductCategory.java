b0b1382d253f1ea2079c1705cd9eb9575422cfa7
William Hsieh <best5946@gmail.com>
2025-08-18 23:52:31 +0800

    商品功能 - Refactor category，使 Enum 類型

---
+++ src/main/java/com/williamhsieh/fruitandessence/constant/ProductCategory.java
@@ -0,0 +1,12 @@
+package com.williamhsieh.fruitandessence.constant;
+
+public enum ProductCategory {
+
+    BERRIES,
+    DRUPES,
+    POME_FRUITS,
+    CITRUS_FRUITS,
+    AGGREGATE_FRUITS,
+    MULTIPLE_FRUITS,
+
+}
