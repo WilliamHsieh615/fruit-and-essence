eaee9fb140f2ca3aac368d4d6270dace10a667a6
William Hsieh <best5946@gmail.com>
2025-08-18 23:11:07 +0800

    商品功能 - 實作查詢商品

---
+++ src/main/java/com/williamhsieh/fruitandessence/dao/ProductDao.java
@@ -0,0 +1,9 @@
+package com.williamhsieh.fruitandessence.dao;
+
+import com.williamhsieh.fruitandessence.model.Product;
+
+public interface ProductDao {
+
+    Product getProductById(Integer productId);
+
+}
---
+++ src/main/java/com/williamhsieh/fruitandessence/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.williamhsieh.fruitandessence.service;
+
+import com.williamhsieh.fruitandessence.model.Product;
+
+public interface ProductService {
+
+    Product getProductById(Integer productId);
+
+}
